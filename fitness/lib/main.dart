9b6fab7d12afb23e84c5a7ec39bb97871dc959f7
Angela <nyabutiangella@gmail.com>
2024-07-12 22:24:44 +0300

    Added a new page

--- fitness/lib/main.dart
+++ fitness/lib/main.dart
@@ -73,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
       page = GeneratorPage();
       break;
     case 1:
-      page = Placeholder(); //placeholder page
+      page = FavoritesPage(); //placeholder page
       break;
     default:
       throw UnimplementedError('no widget for $selectedIndex');
@@ -190,4 +190,31 @@ class BigCard extends StatelessWidget {
       ),
     );
   }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    var appState = context.watch<MyAppState>();
+    if (appState.favourites.isEmpty){
+      return Center(
+        child: Text('No favorites yet'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have' '${appState.favourites.length} favorites:'),
+          ),
+          for (var pair in appState.favourites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          )
+      ],
+    );
+  }
+  
 }
